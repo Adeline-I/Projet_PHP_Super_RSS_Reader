5c18cabc41b757fb4a211404956f3d0c80506595
Xavier <95688089+xavierfoulon@users.noreply.github.com>
2022-03-16 11:24:25 +0100

    Mise à jour2

---
+++ views/pages.php
@@ -0,0 +1,285 @@
+<section class="articleSection">
+    <div class="container-fluid">
+        <div class="row">
+            <!-- Première Thématique -->
+            <div class="col-12 col-lg-4 pt-3 px-4 colBorder">
+                <div>
+                    <div class="row align-items-center pb-3 pb-md-5 pb-lg-3 pb-xl-3 my-3 themeArticleHeader">
+                        <div class="col-12 col-md-7 col-lg-12 col-xxl-8 mb-3">
+                            <h4 class="themeArticle m-0 display-6">Thématique 1</h4>
+                        </div>
+                        <div class="col-12 col-md-5 col-lg-12 col-xxl-4">
+                            <div class="mb-3">
+                                <div class="row align-items-center justify-content-center">
+                                    <div class="col-4 col-md-6 col-lg-4 col-xxl-7 d-flex">
+                                        <select name="numberArticle" id="numberArticle"
+                                            class="form-select form-select-sm">
+                                            <option value="6">6</option>
+                                            <option value="9">9</option>
+                                            <option value="12">12</option>
+                                        </select>
+                                    </div>
+                                    <div class="col-2 col-md-6 col-lg-2 col-xxl-5 p-0">
+                                        <label class="form-label m-0" for="numberArticle">articles</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card mb-3">
+                    <div class="row align-items-center">
+                        <div class="col-4">
+                            <img src="/public/assets/img/Aziri_logo.png" class="img-fluid rounded-start imgArticle"
+                                alt="...">
+                        </div>
+                        <div class="col-8">
+                            <div class="card-header">
+                                <p class="dateArticle mb-2">15/03/2022</p>
+                                <h5 class="card-title titleArticle">Titre de l'article</h5>
+                            </div>
+                            <div class="card-body">
+                                <p class="card-text descriptionArticle">
+                                    Description de l'article
+                                </p>
+                            </div>
+                            <div class="card-footer">
+                                <a href="#" role="button" class="linkArticle">Lire l'article</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card mb-3">
+                    <div class="row align-items-center">
+                        <div class="col-4">
+                            <img src="/public/assets/img/Aziri_logo.png" class="img-fluid rounded-start imgArticle"
+                                alt="...">
+                        </div>
+                        <div class="col-8">
+                            <div class="card-header">
+                                <p class="dateArticle mb-2">15/03/2022</p>
+                                <h5 class="card-title titleArticle">Titre de l'article</h5>
+                            </div>
+                            <div class="card-body">
+                                <p class="card-text descriptionArticle">
+                                    Description de l'article
+                                </p>
+                            </div>
+                            <div class="card-footer">
+                                <a href="#" role="button" class="linkArticle">Lire l'article</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card mb-3">
+                    <div class="row align-items-center">
+                        <div class="col-4">
+                            <img src="/public/assets/img/Aziri_logo.png" class="img-fluid rounded-start imgArticle"
+                                alt="...">
+                        </div>
+                        <div class="col-8">
+                            <div class="card-header">
+                                <p class="dateArticle mb-2">15/03/2022</p>
+                                <h5 class="card-title titleArticle">Titre de l'article</h5>
+                            </div>
+                            <div class="card-body">
+                                <p class="card-text descriptionArticle">
+                                    Description de l'article
+                                </p>
+                            </div>
+                            <div class="card-footer">
+                                <a href="#" role="button" class="linkArticle">Lire l'article</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Deuxième Thématique -->
+            <div class="col-12 col-lg-4 pt-3 px-4 colBorder">
+                <div>
+                    <div class="row align-items-center pb-3 pb-md-5 pb-lg-3 pb-xl-3 my-3 themeArticleHeader">
+                        <div class="col-12 col-md-7 col-lg-12 col-xxl-8 mb-3">
+                            <h4 class="themeArticle m-0 display-6">Thématique 1</h4>
+                        </div>
+                        <div class="col-12 col-md-5 col-lg-12 col-xxl-4">
+                            <div class="mb-3">
+                                <div class="row align-items-center justify-content-center">
+                                    <div class="col-4 col-md-6 col-lg-4 col-xxl-7 d-flex">
+                                        <select name="numberArticle" id="numberArticle"
+                                            class="form-select form-select-sm">
+                                            <option value="6">6</option>
+                                            <option value="9">9</option>
+                                            <option value="12">12</option>
+                                        </select>
+                                    </div>
+                                    <div class="col-2 col-md-6 col-lg-2 col-xxl-5 p-0">
+                                        <label class="form-label m-0" for="numberArticle">articles</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card mb-3">
+                    <div class="row align-items-center">
+                        <div class="col-4">
+                            <img src="/public/assets/img/Aziri_logo.png" class="img-fluid rounded-start imgArticle"
+                                alt="...">
+                        </div>
+                        <div class="col-8">
+                            <div class="card-header">
+                                <p class="dateArticle mb-2">15/03/2022</p>
+                                <h5 class="card-title titleArticle">Titre de l'article</h5>
+                            </div>
+                            <div class="card-body">
+                                <p class="card-text descriptionArticle">
+                                    Description de l'article
+                                </p>
+                            </div>
+                            <div class="card-footer">
+                                <a href="#" role="button" class="linkArticle">Lire l'article</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card mb-3">
+                    <div class="row align-items-center">
+                        <div class="col-4">
+                            <img src="/public/assets/img/Aziri_logo.png" class="img-fluid rounded-start imgArticle"
+                                alt="...">
+                        </div>
+                        <div class="col-8">
+                            <div class="card-header">
+                                <p class="dateArticle mb-2">15/03/2022</p>
+                                <h5 class="card-title titleArticle">Titre de l'article</h5>
+                            </div>
+                            <div class="card-body">
+                                <p class="card-text descriptionArticle">
+                                    Description de l'article
+                                </p>
+                            </div>
+                            <div class="card-footer">
+                                <a href="#" role="button" class="linkArticle">Lire l'article</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card mb-3">
+                    <div class="row align-items-center">
+                        <div class="col-4">
+                            <img src="/public/assets/img/Aziri_logo.png" class="img-fluid rounded-start imgArticle"
+                                alt="...">
+                        </div>
+                        <div class="col-8">
+                            <div class="card-header">
+                                <p class="dateArticle mb-2">15/03/2022</p>
+                                <h5 class="card-title titleArticle">Titre de l'article</h5>
+                            </div>
+                            <div class="card-body">
+                                <p class="card-text descriptionArticle">
+                                    Description de l'article
+                                </p>
+                            </div>
+                            <div class="card-footer">
+                                <a href="#" role="button" class="linkArticle">Lire l'article</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Troisième Thématique -->
+            <div class="col-12 col-lg-4 pt-3 px-4">
+                <div>
+                    <div class="row align-items-center pb-3 pb-md-5 pb-lg-3 pb-xl-3 my-3 themeArticleHeader">
+                        <div class="col-12 col-md-7 col-lg-12 col-xxl-8 mb-3">
+                            <h4 class="themeArticle m-0 display-6">Thématique 1</h4>
+                        </div>
+                        <div class="col-12 col-md-5 col-lg-12 col-xxl-4">
+                            <div class="mb-3">
+                                <div class="row align-items-center justify-content-center">
+                                    <div class="col-4 col-md-6 col-lg-4 col-xxl-7 d-flex">
+                                        <select name="numberArticle" id="numberArticle"
+                                            class="form-select form-select-sm">
+                                            <option value="6">6</option>
+                                            <option value="9">9</option>
+                                            <option value="12">12</option>
+                                        </select>
+                                    </div>
+                                    <div class="col-2 col-md-6 col-lg-2 col-xxl-5 p-0">
+                                        <label class="form-label m-0" for="numberArticle">articles</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card mb-3">
+                    <div class="row align-items-center">
+                        <div class="col-4">
+                            <img src="/public/assets/img/Aziri_logo.png" class="img-fluid rounded-start imgArticle"
+                                alt="...">
+                        </div>
+                        <div class="col-8">
+                            <div class="card-header">
+                                <p class="dateArticle mb-2">15/03/2022</p>
+                                <h5 class="card-title titleArticle">Titre de l'article</h5>
+                            </div>
+                            <div class="card-body">
+                                <p class="card-text descriptionArticle">
+                                    Description de l'article
+                                </p>
+                            </div>
+                            <div class="card-footer">
+                                <a href="#" role="button" class="linkArticle">Lire l'article</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card mb-3">
+                    <div class="row align-items-center">
+                        <div class="col-4">
+                            <img src="/public/assets/img/Aziri_logo.png" class="img-fluid rounded-start imgArticle"
+                                alt="...">
+                        </div>
+                        <div class="col-8">
+                            <div class="card-header">
+                                <p class="dateArticle mb-2">15/03/2022</p>
+                                <h5 class="card-title titleArticle">Titre de l'article</h5>
+                            </div>
+                            <div class="card-body">
+                                <p class="card-text descriptionArticle">
+                                    Description de l'article
+                                </p>
+                            </div>
+                            <div class="card-footer">
+                                <a href="#" role="button" class="linkArticle">Lire l'article</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card mb-3">
+                    <div class="row align-items-center">
+                        <div class="col-4">
+                            <img src="/public/assets/img/Aziri_logo.png" class="img-fluid rounded-start imgArticle"
+                                alt="...">
+                        </div>
+                        <div class="col-8">
+                            <div class="card-header">
+                                <p class="dateArticle mb-2">15/03/2022</p>
+                                <h5 class="card-title titleArticle">Titre de l'article</h5>
+                            </div>
+                            <div class="card-body">
+                                <p class="card-text descriptionArticle">
+                                    Description de l'article
+                                </p>
+                            </div>
+                            <div class="card-footer">
+                                <a href="#" role="button" class="linkArticle">Lire l'article</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
