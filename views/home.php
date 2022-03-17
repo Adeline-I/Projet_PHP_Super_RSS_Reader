b5fb1f0b2c2e1a72d42a6da66c693589594c2b9b
Xavier <95688089+xavierfoulon@users.noreply.github.com>
2022-03-17 09:35:20 +0100

    Mise à jour 3

--- views/home.php
+++ views/home.php
@@ -1,62 +1,43 @@
-
-    <main>
-        <!--Home page with Carousel-->
-        <div class="max-width">
-            <div class="home-content">
-                <p class="text-2 text-center"> Bienvenue sur Aziri ! <br> Venez découvrir notre actualité Gaming</p>
-            </div>
-        </div>
-        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
-                    aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
-                    aria-label="Slide 2"></button>
-                <button type="button" class="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
-                    aria-label="Slide 3"></button>
-            </div>
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img src="https://i.ibb.co/fxHJCWB/e3-2021-hogwarts-legacy.jpg" class="d-block w-100">
-                </div>
-                <div class="carousel-item">
-                    <img src="https://i.ibb.co/fxHJCWB/e3-2021-hogwarts-legacy.jpg" class="d-block w-100">
-                </div>
-                <div class="carousel-item">
-                    <img src="https://i.ibb.co/fxHJCWB/e3-2021-hogwarts-legacy.jpg" class="d-block w-100">
-                </div>
-            </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
-                data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
-                data-bs-slide="next">
-                <span class="carousel-control-next-icon"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
-            <div class="count"></div>
-        </div>
-
-
-    </main>
-    
-    <!-- Modal -->
-    <div class="modal fade mt-5 pt-5" id="staticParams" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
-        aria-labelledby="staticBackdropLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content border-5">
-                <div class="modal-header">
-                    <h5 class="modal-title col-11 d-flex justify-content-center ms-3" id="staticBackdropLabel">
-                        Publication Artisans
-                    </h5>
-                    <i class="bi bi-x-lg col-1" type="button" data-bs-dismiss="modal"></i>
-                </div>
-                <div class="modal-main">
-                    ...
-                </div>
-            </div>
+<main>
+    <!--Home page with Carousel-->
+    <div class="max-width">
+        <div class="home-content">
+            <p class="text-2 text-center"> Bienvenue sur Aziri ! <br> Venez découvrir notre actualité Gaming</p>
         </div>
     </div>
-    
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
+                aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+                aria-label="Slide 2"></button>
+            <button type="button" class="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+                aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="https://i.ibb.co/fxHJCWB/e3-2021-hogwarts-legacy.jpg" class="d-block w-100">
+            </div>
+            <div class="carousel-item">
+                <img src="https://i.ibb.co/fxHJCWB/e3-2021-hogwarts-legacy.jpg" class="d-block w-100">
+            </div>
+            <div class="carousel-item">
+                <img src="https://i.ibb.co/fxHJCWB/e3-2021-hogwarts-legacy.jpg" class="d-block w-100">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="next">
+            <span class="carousel-control-next-icon"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+        <div class="count"></div>
+    </div>
+    <div class="alert text-center">
+        <h6>Sélectionner vos actualités <br>favorites en cliquant sur ⚙️</h6>
+    </div>
+</main>
